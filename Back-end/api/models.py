33e44b1f74f1ace74a38b419ab262a20932731cb
Kgaogelo <kgaogelojoseph33@gmail.com>
2026-05-19 09:19:28 +0200

    updated the backend api for views.py file

--- Back-end/api/models.py
+++ Back-end/api/models.py
@@ -441,7 +441,7 @@ class TactsoCommitteeMember(models.Model):
 
 class AdminStaffMember(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    uid = models.CharField(max_length=255, verbose_name="UID") 
+    uid = models.CharField(max_length=255,unique=False, verbose_name="UID") 
     full_name = models.CharField(max_length=255, verbose_name="Full Name")
     name = models.CharField(max_length=255, verbose_name="First Name")
     surname = models.CharField(max_length=255, verbose_name="Surname")
